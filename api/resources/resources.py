545a4c59fadc0104773c839ba6630b01b5eaece3
alexis <alexisthual@gmail.com>
2018-11-11 20:32:43 +0100

    Refactor Schemas as Flask resource

--- api/resources/resources.py
+++ api/resources/resources.py
@@ -1,36 +1,14 @@
-from flask import jsonify, send_from_directory
+from flask import jsonify, make_response, send_from_directory
 # note: prefer send_from_directory to send_file  as send_file is not secured.
 # From the official documentation : "Please never pass filenames to this function from user sources;
 # you should use send_from_directory() instead."
 from flask_restful import Resource, reqparse
+import json
 import os
+import requests
 import yaml
 
 from api.common.utils import fhir_resource_path
-
-
-class Store(Resource):
-    def post(self):
-
-        parser = reqparse.RequestParser()
-        parser.add_argument('output_format', required=True, type=str, choices=('json', 'yml'))
-        parser.add_argument('fhir_resource_name', required=True, type=str)
-
-        args = parser.parse_args()
-
-        file_path = fhir_resource_path(args['fhir_resource_name'], parent_folder=args['output_format'])
-        if not file_path:
-            return jsonify(
-                {"message": "Fhir resource not found."}
-            )
-
-        folder = os.path.dirname(file_path)
-        file = os.path.basename(file_path)
-        if args['output_format'] == 'json':
-            return jsonify(yaml.load(open(file_path)))
-
-        elif args['output_format'] == 'yml':
-            return send_from_directory(folder, file)
 
 
 class Mapping(Resource):
@@ -52,6 +30,96 @@ class Mapping(Resource):
         folder = os.path.dirname(file_path)
         file = os.path.basename(file_path)
 
+        if args['output_format'] == 'json':
+            return jsonify(yaml.load(open(file_path)))
+
+        elif args['output_format'] == 'yml':
+            return send_from_directory(folder, file)
+
+
+class Schemas(Resource):
+    '''
+    Restful API dealing with databases schemas.
+    '''
+
+    def __init__(self):
+        '''
+        Inits instance parameters.
+        '''
+
+        self.url = 'http://127.0.0.1:8421'
+
+    def get(self, **kwargs):
+        '''
+        Calls resource's method depending on the number
+        of arguemnts.
+        '''
+
+        if len(kwargs) == 0:
+            return self.get_list()
+        elif len(kwargs) == 2:
+            return self.get_schema(**kwargs)
+
+        return jsonify({
+            'message': 'Invalid number of arguments',
+        })
+
+    def get_list(self):
+        '''
+        Fetches CSV from distant source and builds
+        a flask response.
+        '''
+
+        content = requests.get('{}/databases.csv'.format(
+            self.url
+        )).content
+
+        response = make_response(content)
+        cd = 'attachment; filename=databases.csv'
+        response.headers['Content-Disposition'] = cd
+        response.mimetype = 'text/csv'
+
+        return response
+
+    def get_schema(self, database_name, extension):
+        '''
+        Fetches distant file and parses it according
+        to its extension.
+        '''
+
+        content = requests.get('{}/{}.{}'.format(
+            self.url,
+            database_name,
+            extension
+        )).content
+
+        if extension == 'json':
+            return jsonify(json.loads(content))
+        elif extension == 'yml':
+            return jsonify(yaml.load(content))
+
+        return jsonify({
+            'message': 'Unknown extension.'
+        })
+
+
+class Store(Resource):
+    def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('output_format', required=True, type=str, choices=('json', 'yml'))
+        parser.add_argument('fhir_resource_name', required=True, type=str)
+
+        args = parser.parse_args()
+
+        file_path = fhir_resource_path(args['fhir_resource_name'], parent_folder=args['output_format'])
+        if not file_path:
+            return jsonify(
+                {"message": "Fhir resource not found."}
+            )
+
+        folder = os.path.dirname(file_path)
+        file = os.path.basename(file_path)
         if args['output_format'] == 'json':
             return jsonify(yaml.load(open(file_path)))
 
--- server.py
+++ server.py
@@ -1,31 +1,20 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 import json
-import requests
 
-from api.resources import Mapping, Store
+from api.resources import Mapping, Schemas, Store
 
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Mapping, '/mapping')
+api.add_resource(Schemas,
+    '/schemas/<database_name>/<extension>',
+    '/schemas/list',
+)
+# api.add_resource(Schemas, '/schemas')
 api.add_resource(Store, '/store')
-
-@app.route('/schemas/<database_name>/<extension>', methods=['GET'])
-def get_schema(database_name, extension):
-
-    content = requests.get('http://127.0.0.1:8421/{}.{}'.format(
-        database_name, extension
-    )).content
-
-    return jsonify(json.loads(content))
-
-@app.route('/')
-def index():
-    return jsonify({
-        'message': 'Welcome to Fhir API',
-    })
 
 
 if __name__ == '__main__':
